b6d85ac0eb8a61503db420686c24676aa0e69214
Jost221 <mr.z.75@mail.ru>
2022-02-26 05:40:00 +0300

    0.15 line_equation

--- line_equation.py
+++ line_equation.py
@@ -4,33 +4,40 @@ import importlib
 import os
 import math
 
-def gaus(equations, e=0, elements = 1):
+def gaus(equations=[], e=0, elements = 0, last_result=[]):
     result = []
     x = 1
     simbol = '0123456789.-'
-    switch = False
-    # signs = '+-/*='
-    for i  in range(len(equations)):
-        value = ''
-        line = []
-        if i == 0:
-            line.append('')
-        for z in range(len(equations[i])):
-            if equations[i][z] in simbol:
-                value+=equations[i][z]
-            elif equations[i][z] == 'x':
-                line.append(float(value))
-                value= ''
-            if z == len(equations[i])-1:
-                line.append(float(value))
-                value = ''
-        if i != 0:
-            line.insert(0, general_functions.round_function(-line[0]/result[0][1], len(str(e))-2))
-        # добавить контрольные суммы
-        # if elements == 0:
-        #     line.append("")
+    if elements == 0:
+        for i  in range(len(equations)):
+            value = ''
+            line = []
+            if i == 0:
+                line.append('')
+            for z in range(len(equations[i])):#  x1   x2   xn   свободные_члены 
+                if equations[i][z] in simbol:
+                    value+=equations[i][z]
+                elif equations[i][z] == 'x':
+                    line.append(float(value))
+                    value= ''
+                if z == len(equations[i])-1:
+                    line.append(float(value))
+                    value = ''
+            if i != 0:
+                line.insert(0,
+                general_functions.round_function(-line[0]/result[0][1], len(str(e))-2))#m
+            #контрольные суммы, довести до естественного вида
+            line.append("")
+    else:
+        pass    #подсчёт 2ого блока 
+    if elements != 0:
+        line.append(general_functions.round_function(last_result[0][-2]*last_result[1][0]+last_result[1][-2])) 
         result.append(line)
-
+        #Рекурсия для блоков таблицы
+    for i in equations:
+        if i[-1] == '' or i[-1] > e:
+            result.append(gaus(e=e, elements=elements+1, last_result = result))
+            break
     return result
 
 if __name__ == '__main__':
@@ -38,6 +45,6 @@ if __name__ == '__main__':
         '0.68x+0.75x-0.5x=0',
         '0.21x+2x-12x=0'
     ]
-    printing = gaus(equations)
+    printing = gaus(equations=equations)
     print(printing)
 
